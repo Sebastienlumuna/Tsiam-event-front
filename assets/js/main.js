f1edc7a8258a98fcd62fccff6aa8293d355bfb67
Sebastienlumuna <sebastienlumuna8@gmail.com>
2025-02-07 19:28:35 +0100

    avancement du lading page

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,3 +207,10 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+
+document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+  faqItem.addEventListener('click', () => {
+    faqItem.parentNode.classList.toggle('faq-active');
+  });
+});
